40a65b43e3b9b115d8d52f6f4d0593016b6229c5
Julien <minimoon300@gmail.com>
2022-05-27 05:54:11 +0900

    MOD reformat snake file structure

---
+++ snake_settings.py
@@ -0,0 +1,24 @@
+import os.path
+import random
+import pygame
+import sys
+import pickle as pk1
+import os.path as exists
+
+# settings
+grid_cell_size = 20
+single_player_grid_width = 40
+single_player_grid_height = 40
+dual_player_grid_width = 80
+dual_player_grid_height = 40
+# grid_size = 40
+DIR_UP = [-1, 0]
+DIR_DOWN = [1, 0]
+DIR_LEFT = [0, -1]
+DIR_RIGHT = [0, 1]
+FPS = 60
+snake_speed = 5
+
+username = ""
+fpsClock = pygame.time.Clock()
+screen = pygame.display.set_mode([800, 800])
